b2a55c30fd6ba6dfcff383fdd756e3a7070a3afc
Qingxiao Dong <qingxiao@seas.upenn.edu>
2016-12-01 21:32:30 -0500

    storage service framework

---
+++ grpc/storage_query/storage_client.cc
@@ -0,0 +1,14 @@
+#include <grpc++/grpc++.h>
+#include "storage_query.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+using storagequery::StorageQuery;
+
+class StorageClient {
+
+};
+
+int main(int argc, char** argv) {	
+}
---
+++ grpc/storage_query/storage_server.cc
@@ -0,0 +1,17 @@
+#include <grpc++/grpc++.h>
+
+#include "storage_query.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using storagequery::StorageQuery;
+
+class StorageServer {
+
+};
+
+
+int main(int argc, char** argv) {
+}
