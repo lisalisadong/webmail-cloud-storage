047fd6d6ffc05c41dc67e21872f1edeab19f15ee
Qingxiao Dong <qingxiao@seas.upenn.edu>
2016-12-01 21:31:54 -0500

    try helloworld

--- grpc/helloworld/greeter_client.cc
+++ grpc/helloworld/greeter_client.cc
@@ -78,6 +78,24 @@ class GreeterClient {
     }
   }
 
+  std::string SayHelloAgain(const std::string& user) {
+    HelloRequest request;
+    request.set_name(user);
+
+    HelloReply reply;
+    ClientContext context;
+
+    Status status = stub_->SayHelloAgain(&context, request, &reply);
+
+    if (status.ok()) {
+      return reply.message();
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      return "RPC failed";
+    }
+  }
+
  private:
   std::unique_ptr<Greeter::Stub> stub_;
 };
@@ -91,6 +109,9 @@ int main(int argc, char** argv) {
       "localhost:50051", grpc::InsecureChannelCredentials()));
   std::string user("world");
   std::string reply = greeter.SayHello(user);
+  std::cout << "Greeter received: " << reply << std::endl;
+
+  reply = greeter.SayHelloAgain(user);
   std::cout << "Greeter received: " << reply << std::endl;
 
   return 0;
