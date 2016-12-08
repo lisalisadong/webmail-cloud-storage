d24ee414cc1fb6851d824053d1c5ce78dcf92c9f
Qingxiao Dong <qingxiao@seas.upenn.edu>
2016-12-07 22:45:57 -0500

    basic functions of file system

--- backend/storage_query/file_system.cc
+++ backend/storage_query/file_system.cc
@@ -28,19 +28,29 @@ std::string FileSystem::serialize(std::string str) {
 std::unordered_map<std::string, std::unordered_map<std::string, std::string> > FileSystem::read(std::string row, std::string col) {
 	std::string fileName = this->keys_to_file(row, col);
 	std::ifstream file (fileName);
-	std::unordered_map<std::string, std::unordered_map<std::string, std::string> > ret;
+	std::unordered_map<std::string, std::unordered_map<std::string, std::string> > map;
 	if (file.is_open()) {
-
+		std::string tuple;
+		while (true) {
+			tuple = get_next_tuple(file);
+			if (tuple.length() == 0)
+				break;
+			int pos = 0;
+			std::string row = deserialize_next(tuple, pos);
+			std::string col = deserialize_next(tuple, pos);
+			std::string val = deserialize_next(tuple, pos);
+			map[row][col] = val;
+		}
 	}
 	else std::cout << "Cannot open file to read!" << std::endl; //TODO: write loggers in utils
-	return ret;
+	return map;
 }
 
 void FileSystem::write(std::string row, std::string col, std::string val) {
 	std::string raw = serialize(row) + serialize(col) + serialize(val);
 	std::string tuple = serialize(raw);
 	std::string fileName = this->keys_to_file(row, col);
-	std::ofstream file (fileName);
+	std::ofstream file (fileName, std::ios_base::app);
 	if (file.is_open()) {
 	    file << tuple;
 	    file.close();
@@ -49,7 +59,7 @@ void FileSystem::write(std::string row, std::string col, std::string val) {
 }
 
 std::string FileSystem::keys_to_file(std::string row, std::string col) {
-	return "";
+	return row + col;
 }
 
 std::string FileSystem::get_next_tuple(std::ifstream& stream) {
@@ -60,21 +70,48 @@ std::string FileSystem::get_next_tuple(std::ifstream& stream) {
 			break;
 		lenStr += c;
 	}
-	int len = stoi(lenStr);
+	if (lenStr.length() == 0) {
+		return "";
+	}
+	int len = std::stoi(lenStr);
 	char buf[len + 1];
 	stream.read(buf, len);
+	buf[len] = '\0';
 	std::string ret(buf);
 	return ret;
 }
 
-void FileSystem::deserialize(std::string str, std::string& row, std::string& col, std::string& val) {
-	std::size_t found = str.find_first_of(":");
-	return;
+std::string FileSystem::deserialize_next(std::string str, int& pos) {
+	std::size_t found = str.find(":", pos);
+	std::string lenStr = str.substr(pos, found - pos);
+	int len = std::stoi(lenStr);
+	std::string ret = str.substr(found + 1, len);
+	pos = found + 1 + len;
+	return ret;
 }
 
-int main() {
-	return 0;
-}
+
+
+// int main() {
+// 	FileSystem fs;
+// 	fs.write("lisa", "email", "123");
+// 	fs.write("lis", "aemail", "456");
+// 	fs.write("qingxiao", "drive", "xxx");
+// 	std::unordered_map<std::string, std::unordered_map<std::string, std::string> > map = fs.read("lisa", "email");
+// 	for(std::unordered_map<std::string, std::unordered_map<std::string, std::string> >::const_iterator it = map.begin(); it != map.end(); it++) {
+// 		for (std::unordered_map<std::string, std::string>::const_iterator it2 = it->second.begin(); it2 != it->second.end(); it2++) {
+// 			std::cout << it->first << "," << it2->first << "," << it2->second << std::endl;
+// 		}
+// 	}
+
+// 	map = fs.read("qingxiao", "drive");
+// 	for(std::unordered_map<std::string, std::unordered_map<std::string, std::string> >::const_iterator it = map.begin(); it != map.end(); it++) {
+// 		for (std::unordered_map<std::string, std::string>::const_iterator it2 = it->second.begin(); it2 != it->second.end(); it2++) {
+// 			std::cout << it->first << "," << it2->first << "," << it2->second << std::endl;
+// 		}
+// 	}
+// 	return 0;
+// }
 
 
 
