926af07e549757d41d750767d1b7069e1b632df9
Xiaofan Dou <xiaofand@seas.upenn.edu>
2016-12-10 17:39:08 -0500

    merge

--- backend/storage_query/file_system.h
+++ backend/storage_query/file_system.h
@@ -22,7 +22,7 @@ public:
 	
 	void file_to_keys(std::unordered_map<std::string, std::unordered_set<std::pair<std::string, std::string>, Hash> >& map);
 
-	void get_mappings(std::unordered_map<std::string, std::vector<std::pair<std::string, std::string> > > fileToKey, std::unordered_map<std::string, std::unordered_map<std::string, std::string> > keyToFile);
+	void get_mappings(std::unordered_map<std::string, std::vector<std::pair<std::string, std::string> > > &fileToKey, std::unordered_map<std::string, std::unordered_map<std::string, std::string> > &keyToFile);
 
 private:
 	std::string serialize(std::string str);
