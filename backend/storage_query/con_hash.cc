d7260398d05f9da431dc299a2fe4f1d3ede370fb
Xiaofan Dou <xiaofand@seas.upenn.edu>
2016-12-13 21:46:46 -0500

    consistent hash version 1.0 finished

--- backend/storage_query/con_hash.cc
+++ backend/storage_query/con_hash.cc
@@ -1,15 +1,23 @@
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <limits.h>
+#include <vector>
+#include <map>
+#include <unordered_set>
+#include "Conhash.h"
 
-// int main() {
+int main() {
 
-// 	Hasher hasher;
+	Hasher hasher;
 
-// 	long hashVal = hasher.getHashVal("127.0.0.1:8000");
-// 	std::cout << hashVal << std::endl;
+	long hashVal = hasher.getHashVal("127.0.0.1:8000");
+	std::cout << hashVal << std::endl;
 
-// 	hashVal = hasher.getHashVal("127.0.0.1:8001");
-// 	std::cout << hashVal << std::endl;
+	hashVal = hasher.getHashVal("127.0.0.1:8001");
+	std::cout << hashVal << std::endl;
 
-// 	hashVal = hasher.getHashVal("127.0.0.1:8002");
-// 	std::cout << hashVal << std::endl;
+	hashVal = hasher.getHashVal("127.0.0.1:8002");
+	std::cout << hashVal << std::endl;
 	
-// }
+}
